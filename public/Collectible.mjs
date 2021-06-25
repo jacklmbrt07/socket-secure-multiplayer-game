918dbf0ad63cf86e476e8dc80cdeea864a638d6c
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-06-25 13:14:07 -0700

    added code

--- public/Collectible.mjs
+++ public/Collectible.mjs
@@ -1,8 +1,22 @@
 class Collectible {
-  constructor({x, y, value, id}) {
-
+  constructor({ x = 10, y = 10, w = 15, h = 15, value = 1, id }) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.value = value;
+    this.id = id;
   }
 
+  draw(context, imgObj) {
+    if (this.value === 1) {
+      context.drawImage(imgObj.bronzeCoinArt, this.x, this.y);
+    } else if (this.value === 2) {
+      context.drawImage(imgObj.silverCoinArt, this.x, this.y);
+    } else {
+      context.drawImage(imgObj.goldCoinArt, this.x, this.y);
+    }
+  }
 }
 
 /*
@@ -11,6 +25,6 @@ class Collectible {
 */
 try {
   module.exports = Collectible;
-} catch(e) {}
+} catch (e) {}
 
 export default Collectible;
--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 require("dotenv").config();
-const portNum = process.env.PORT || 3000;
-
 const express = require("express");
-const http = require("http");
-const path = require("path");
-const socketIO = require("socket.io");
 const bodyParser = require("body-parser");
-const expect = require("chai");
+const socket = require("socket.io");
+const helmet = require("helmet");
+const nocache = require("nocache");
+const cors = require('cors');
 
 const fccTestingRoutes = require("./routes/fcctesting.js");
 const runner = require("./test-runner.js");
 
 const app = express();
-const server = http.Server(app);
-const io = socketIO(server);
 
-app.set("port", portNum);
+app.use(
+  helmet({
+    noSniff: true,
+    xssFilter: true,
+    hidePoweredBy: {
+      setTo: "PHP 7.4.3",
+    }
+  })
+);
+
+app.use(nocache());
+
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
+
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use("/assets", express.static(process.cwd() + "/assets"));
 
@@ -35,8 +44,10 @@ app.use(function (req, res, next) {
   res.status(404).type("text").send("Not Found");
 });
 
+const portNum = process.env.PORT || 3000;
+
 // Set up server and tests
-server.listen(portNum, () => {
+const server = app.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
   if (process.env.NODE_ENV === "test") {
     console.log("Running Tests...");
@@ -51,44 +62,114 @@ server.listen(portNum, () => {
   }
 });
 
-io.on("connection", (socket) => {});
+// Socket.io setup:
+// Start app and bind
+// Socket.io to the same port
+const io = socket(server);
+const Collectible = require("./public/Collectible");
+const { generateStartPos, canvasCalcs } = require("./public/canvas-data");
 
-setInterval(() => {
-  io.sockets.emit("message", "hi!");
-}, 1000);
+let currPlayers = [];
+const destroyedCoins = [];
 
-var players = {};
-io.on("connection", (socket) => {
-  socket.on("new player", () => {
-    console.log("Player connected: ", socket.id)
-    players[socket.id] = {
-      x: 300,
-      y: 300,
-    };
+const generateCoin = () => {
+  const rand = Math.random();
+  let coinValue;
+
+  if (rand < 0.6) {
+    coinValue = 1;
+  } else if (rand < 0.85) {
+    coinValue = 2;
+  } else {
+    coinValue = 3;
+  }
+
+  return new Collectible({
+    x: generateStartPos(
+      canvasCalcs.playFieldMinX,
+      canvasCalcs.playFieldMaxX,
+      5
+    ),
+    y: generateStartPos(
+      canvasCalcs.playFieldMinY,
+      canvasCalcs.playFieldMaxY,
+      5
+    ),
+    value: coinValue,
+    id: Date.now(),
   });
-  socket.on("movement", (data) => {
-    var player = players[socket.id] || {};
-    if (data.left) {
-      player.x -= 5;
-    }
-    if (data.right) {
-      player.x += 5;
-    }
-    if (data.up) {
-      player.y += 5;
-    }
-    if (data.down) {
-      player.y -= 5;
+};
+
+let coin = generateCoin();
+
+io.sockets.on("connection", (socket) => {
+  console.log(`New connection ${socket.id}`);
+
+  socket.emit("init", { id: socket.id, players: currPlayers, coin });
+
+  socket.on("new-player", (obj) => {
+    obj.id = socket.id;
+    currPlayers.push(obj);
+    socket.broadcast.emit("new-player", obj);
+  });
+
+  socket.on("move-player", (dir, obj) => {
+    const movingPlayer = currPlayers.find((player) => player.id === socket.id);
+    if (movingPlayer) {
+      movingPlayer.x = obj.x;
+      movingPlayer.y = obj.y;
+
+      socket.broadcast.emit("move-player", {
+        id: socket.id,
+        dir,
+        posObj: { x: movingPlayer.x, y: movingPlayer.y },
+      });
     }
   });
-  socket.on('disconnect', () => {
-    console.log('player disconnected')
-  })
+
+  socket.on("stop-player", (dir, obj) => {
+    const stoppingPlayer = currPlayers.find(
+      (player) => player.id === socket.id
+    );
+    if (stoppingPlayer) {
+      stoppingPlayer.x = obj.x;
+      stoppingPlayer.y = obj.y;
+
+      socket.broadcast.emit("stop-player", {
+        id: socket.id,
+        dir,
+        posObj: { x: stoppingPlayer.x, y: stoppingPlayer.y },
+      });
+    }
+  });
+
+  socket.on("destroy-item", ({ playerId, coinValue, coinId }) => {
+    if (!destroyedCoins.includes(coinId)) {
+      const scoringPlayer = currPlayers.find((obj) => obj.id === playerId);
+      const sock = io.sockets.connected[scoringPlayer.id];
+
+      scoringPlayer.score += coinValue;
+      destroyedCoins.push(coinId);
+
+      // Broadcast to all players when someone scores
+      io.emit("update-player", scoringPlayer);
+
+      // Communicate win state and broadcast losses
+      if (scoringPlayer.score >= 100) {
+        sock.emit("end-game", "win");
+        sock.broadcast.emit("end-game", "lose");
+      }
+
+      // Generate new coin and send it to all players
+      coin = generateCoin();
+      io.emit("new-coin", coin);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("remove-player", socket.id);
+    currPlayers = currPlayers.filter((player) => player.id !== socket.id);
+  });
 });
-
-setInterval(() => {
-  io.sockets.emit("state", players);
-}, 1000 / 60);
-
 
 module.exports = app; // For testing
